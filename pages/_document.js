f912c7056ef165fde91cb60c8722256d2e7a472f
amine alaoui <omari1amineco@gmail.com>
2023-12-31 16:06:18 +0100

    change the title of the page

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>User Flow</title>
+        <title>USER CARD</title>
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
