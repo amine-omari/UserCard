6734634f3f10e03677efbf57d7be461b32139ecc
amine alaoui <omari1amineco@gmail.com>
2023-12-31 17:02:54 +0100

    feat: add apiUrl for random user data

--- components/MainContainer.jsx
+++ components/MainContainer.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 const MainContainer = () => {
+  const [user, setUser] = useState();
+  const apiUrl = "https://randomuser.me/api/";
+
   return (
     <div className="w-[90%] max-w-[470px] text-white mt-20 mx-auto rounded-3xl py-10 px-7 text-center bg-gradient-to-br from-teal-400 via-teal-500 to-purple-800 space-y-10">
       <div>
@@ -10,7 +13,11 @@ const MainContainer = () => {
       </div>
       <div className="flex flex-col items-center space-y-5">
         <div className="hover:scale-105 cursor-pointer transition duration-500">
-          <img src="/imgs/nddj65fb.png" alt="user img" className="w-32 h-32 rounded-full" />
+          <img
+            src="/imgs/nddj65fb.png"
+            alt="user img"
+            className="w-32 h-32 rounded-full"
+          />
         </div>
         <div className="space-y-5 font-semibold">
           <h1 className="uppercase text-3xl">amine</h1>
