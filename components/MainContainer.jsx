910c2186a68a70baf5b90f9b33273447b536708c
amine alaoui <omari1amineco@gmail.com>
2023-12-31 16:27:03 +0100

    feat: add the button to the page

--- components/MainContainer.jsx
+++ components/MainContainer.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const MainContainer = () => {
   return (
-    <div>MainContainer</div>
-  )
-}
+    <div className="w-[90%] max-w-[470px] text-white mt-20 mx-auto rounded-3xl py-10 px-7 text-center bg-gradient-to-br from-teal-400 via-teal-500 to-purple-800">
+      <div>
+        <button className="bg-white text-black py-2 px-8 rounded-full hover:shadow-2xl hover:opacity-95 border border-black transition duration-300 hover:scale-105 font-medium">
+          Get a New User
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default MainContainer
+export default MainContainer;
