75e3923c2c5407fe88219a6f8653e0b641e69f18
amine alaoui <omari1amineco@gmail.com>
2024-01-01 00:48:33 +0100

    fix: MainContainer component

--- components/MainContainer.jsx
+++ components/MainContainer.jsx
@@ -8,35 +8,51 @@ const MainContainer = () => {
   const searchUser = async () => {
     try {
       const response = await fetch(apiUrl);
-      const userData = response.json();
+      const userData = await response.json();
       console.log(userData);
 
-      setUser(userData);
+      setUser(userData.results[0]);
     } catch (error) {
       console.error("error fetching random user:", error);
     }
   };
 
   return (
-    <div className="w-[90%] max-w-[470px] text-white mt-20 mx-auto rounded-3xl py-10 px-7 text-center bg-gradient-to-br from-teal-400 via-teal-500 to-purple-800 space-y-10">
+    <div
+      className={`${bgColor} w-[90%] max-w-[470px] text-white mt-20 mx-auto rounded-3xl py-10 px-7 text-center space-y-10`}
+    >
       <div>
-        <button className="bg-white text-black py-2 px-8 rounded-full hover:shadow-2xl hover:opacity-95 border border-black transition duration-300 hover:scale-105 font-medium">
+        <button
+          onClick={searchUser}
+          className="bg-white text-black py-2 px-8 rounded-full hover:shadow-2xl hover:opacity-95 border border-black transition duration-300 hover:scale-105 font-medium"
+        >
           Get a New User
         </button>
       </div>
-      <div className="flex flex-col items-center space-y-5">
-        <div className="hover:scale-105 cursor-pointer transition duration-500">
-          <img
-            src="/imgs/nddj65fb.png"
-            alt="user img"
-            className="w-32 h-32 rounded-full"
-          />
+      {user && (
+        <div className="flex flex-col items-center space-y-5">
+          <div className="hover:scale-105 cursor-pointer transition duration-500">
+            <img
+              src={user.picture.large}
+              alt="user img"
+              className="w-32 h-32 rounded-full"
+            />
+          </div>
+          <div className="space-y-5 font-medium text-xl">
+            <div className="flex justify-center space-x-2">
+              <p>Age:</p>
+              <p>{user.dob.age} </p>
+            </div>
+            <div className="flex space-x-2">
+              <p>Full Name:</p>
+              <h1 className="uppercase">
+                {user.name.first} {user.name.last}
+              </h1>
+            </div>
+            <p className="text-gray-200 text-xl">+212 {user.phone}</p>
+          </div>
         </div>
-        <div className="space-y-5 font-semibold">
-          <h1 className="uppercase text-3xl">amine</h1>
-          <p className="text-gray-200">Phone N: +67326573675</p>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
